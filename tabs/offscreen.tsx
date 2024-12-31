e23afbec25afeab57df10e832994f4331b110fc3
softwaredev <vikasgupta@zestgeek.com>
2024-12-31 16:28:31 +0530

    now upload chunk calling on when windo and ffmpeg is rerady

--- tabs/offscreen.tsx
+++ tabs/offscreen.tsx
@@ -29,6 +29,7 @@ const OffScreen = () => {
   const [camOnlyStream, setCamOnlyStream] = useState(null)
   const [isDiscardRecording, setIsDiscardRecording] = useState(false)
   const [isWindowOnlyRecording, setIsWindowOnlyRecording] = useState(false)
+  const [uploadChunksCall, setUploadChunksCall] = useState(false)
   const videoRef = useRef(null)
   const [base64Data, setBase64Data] = useState(null)
   const [isPreviewOpened, setIsPreviewOpened] = useState(false)
@@ -79,13 +80,14 @@ const OffScreen = () => {
     if (isPreviewOpened) {
       if (base64Data) {
         console.log("base64Data1121", base64Data)
-        setTimeout(() => {
+        if(uploadChunksCall){
+          uploadChunks()
           setIsPreviewOpened(false)
-        }, 500)
-        uploadChunks()
+          setUploadChunksCall(false)
+        }
       }
     }
-  }, [isPreviewOpened, base64Data])
+  }, [isPreviewOpened, base64Data, uploadChunksCall])
 
   const resetAll = async (showVideo?: any) => {
     if (showVideo !== "restart_camonly") {
@@ -722,6 +724,10 @@ const OffScreen = () => {
             sendResponse("close")
           }
             break;
+          case "START_UPLOAD_CHUNKS": {
+            setUploadChunksCall(true)
+          }
+          break;
           case "RESTART_MICONLY_RECORDING": {
             // camOnlyChunks = []
             isMicOnlyRecordingDiscarded = true
--- tabs/previewContext.tsx
+++ tabs/previewContext.tsx
@@ -454,6 +454,7 @@ export function PreviewProvider({ children }: { children: React.ReactNode }) {
             if (message.type === "ffmpeg-loaded") {
                 setIsFfmpegLoaded(true)
                 console.log("ffmpeg-loaded Call from Demo!!")
+                chrome.runtime.sendMessage({type: "START_UPLOAD_CHUNKS"})
             }
             if (message.type === "ffmpeg-load-error") {
                 console.log("ffmpeg-load-error==>", message)
