962e61e683e8e23a821f99374ee65d4c0ba20b0d
softwaredev <vikasgupta@zestgeek.com>
2025-01-26 12:23:06 +0530

    fixed tailwind config to not include node modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   mode: "jit",
   darkMode: "class",
-  content: ["./**/*.tsx"],
+  content: [
+    "./popup.tsx",
+    "./tabs/**/*.{js,jsx,ts,tsx}", // Target only files in the src folder
+    "./sandboxes/**/*.{js,jsx,ts,tsx}", // Target only files in the src folder
+    "./components/**/*.{js,jsx,ts,tsx}", // Add other folders as needed
+    "./contents/**/*.{js,jsx,ts,tsx}" // If using Next.js or similar
+  ],
   plugins: []
 }
